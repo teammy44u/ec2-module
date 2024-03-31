c4bea743727aff75ca183be6d107f3ddcb8d4f4d
jayjay~ <oluwatimilehinajayi@yahoo.com>
2024-03-31 00:50:01 -0400

    module

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    description = "Amazon machine image"
+    type = string
+    default = "ami-033a1ebf088e56e81"
+  
+}
+variable "instance_type" {
+    default = "t2.micro"
+  
+}
+variable "region_name" {
+    default = "us-east-1"
+  
+}
+variable "profile_name" {
+    default = "default"
+  
+}
+
+  
